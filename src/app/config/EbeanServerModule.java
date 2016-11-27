93be969e18eba38116bfb71a9e59e6b69efc79d0
sadai.sarmiento <sadai.sarmiento@verizondigitalmedia.com>
2016-11-26 23:51:25 -0800

    added model

---
+++ src/app/config/EbeanServerModule.java
@@ -0,0 +1,7 @@
+package modules;
+import com.google.inject.*;
+public class EbeanServerDi extends AbstractModule {
+    public void configure() {
+        //your bindings should come here
+    }
+}
---
+++ src/app/config/EbeanServerProvider.java
@@ -0,0 +1,7 @@
+package config;
+
+/**
+ * Created by scarneiro on 11/26/2016.
+ */
+public class EbeanServerProvider {
+}
